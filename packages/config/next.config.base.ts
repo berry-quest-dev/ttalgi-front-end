91b191bd95e19e1712276adbd095b0657338a8f9
Lee Ye Seo <rosefor2ne@gmail.com>
2025-04-06 17:00:48 +0900

    config: tsconfig jsx preserve로 수정

--- packages/config/next.config.base.ts
+++ packages/config/next.config.base.ts
@@ -19,26 +19,3 @@ const baseConfig: NextConfig = {
 };
 
 export default withTranspile(baseConfig);
-
-
-// import withTM from "next-transpile-modules";
-
-// const nextConfig = withTM(["@packages/ui"])({
-//   reactStrictMode: true,
-//   webpack: (config) => {
-//     config.resolve.alias = {
-//       ...config.resolve.alias,
-//       react: require.resolve("react"),
-//       "react-dom": require.resolve("react-dom"),
-//       "react/jsx-runtime": require.resolve("react/jsx-runtime"),
-//       "react/jsx-dev-runtime": require.resolve("react/jsx-dev-runtime"),
-//       react: path.resolve(__dirname, "../../node_modules/react"),
-//       "react-dom": path.resolve(__dirname, "../../node_modules/react-dom"),
-//       "react/jsx-runtime": path.resolve(__dirname, "../../node_modules/react/jsx-runtime"),
-//       "react/jsx-dev-runtime": path.resolve(__dirname, "../../node_modules/react/jsx-dev-runtime"),
-//     };
-//     return config;
-//   },
-// });
-
-// export default nextConfig;
