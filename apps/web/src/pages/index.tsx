473973f9037f9117c5f869b37131147b8dd73842
Lee Ye Seo <rosefor2ne@gmail.com>
2025-04-28 09:34:50 +0900

    feat: 기본 화면 진입 구현 (스플래쉬 1.5s 후 map 리디랙션 추가)

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { logo } from "@packages/image";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <div>스플래시 (위치 권한 확인 후 이동)</div>;
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/map");
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-white text-center">
+      <h1 className="text-2xl font-semibold mb-4">
+        달콤한 딸기, <br />내 주변 핫한 카페에서
+      </h1>
+      <img src={logo.src} alt="로고" className="w-32 h-32" />
+    </div>
+  );
 }
